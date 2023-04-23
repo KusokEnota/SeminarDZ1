ed5713b88078a4ee567ec59d209bfa02e100d120
KusokEnota <enimausy@gmail.com>
2023-04-23 23:13:04 +0300

    vipolneno dz + 1 dop

--- 3.py
+++ 3.py
@@ -3,3 +3,4 @@ nn = int(str(n) + str(n))
 nnn = int(str(n) + str(n) + str(n))
 result = n + nn + nnn
 print(f"сумма чисел {n},{nn},{nnn} = {result}")
+#Задание 3
--- 5.py
+++ 5.py
@@ -2,3 +2,4 @@ chislo = int(input("Введите трехзначное число: "))
 numb = str(chislo)
 summa = int(numb[0]) + int(numb[1]) + int(numb[2])
 print(f"{chislo} -> {summa} ({numb[0]} + {numb[1]} + {numb[2]})")
+#Задание доп1
